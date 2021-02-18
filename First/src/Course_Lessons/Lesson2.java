5146ceccd761aa8417e09c88e2d4bfb90bb189c9
Arsen Savshak <arsenko525@gmail.com>
2021-02-18 23:15:03 +0200

    New update

---
+++ First/src/Course_Lessons/Lesson2.java
@@ -0,0 +1,63 @@
+package Course_Lessons;
+
+public class Lesson2 {
+	public static void main(String[] args) {
+		byte a1 = 0B1100;
+		byte a2 = 12;
+		byte a3 = 014;
+		byte a4 = 0xc;
+		System.out.println(a1);
+		System.out.println(a2);
+		System.out.println(a3);
+		System.out.println(a4);
+		
+		short b1 = 0B10100010100;
+		short b2 = 1300;
+		short b3 = 02424;
+		short b4 = 0x514;
+		System.out.println(b1);
+		System.out.println(b2);
+		System.out.println(b3);
+		System.out.println(b4);
+		
+		int c1 = 0B0;
+		int c2 = 0;
+		int c3 = 00;
+		int c4 = 0x0;
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		System.out.println(c4);
+		
+		long d1 = 0B0111010110111100110100010101;
+		long d2 = 123456789;
+		long d3 = 0726746425;
+		long d4 = 0x75BCD15;
+		System.out.println(d1);
+		System.out.println(d2);
+		System.out.println(d3);
+		System.out.println(d4);
+		
+		float x1 = 14.1211f;
+		float x2 = 25.2122f;
+		System.out.println(x1);
+		System.out.println(x2);
+		
+		double z1 = 24.1211;
+		double z2 = 552.2221124;
+		System.out.println(z1);
+		System.out.println(z2);
+		
+		boolean q1 = true;
+		boolean q2 = false;
+		System.out.println(q1);
+		System.out.println(q2);
+		
+		
+		char m1 = 1211;
+		char m2 = '\u1234';
+		System.out.println(m1);
+		System.out.println(m2);
+		
+	}
+}
---
+++ First/src/Course_Lessons/weqq.java
@@ -0,0 +1,30 @@
+package Course_Lessons;
+
+public class weqq {
+	public static void main(String[] args) {
+		int i1 = 5;
+		int i2 = 11;
+		double d1 = 5.5;
+		double d2 = 1.3;
+		long l = 20l;
+		double result = 0;
+		result = i2 / d1 + d2 % i1 - l;
+		System.out.println(result);
+		
+		int i = 11;
+		int c = 3;
+		
+		int celoe = 11/3;
+		int ostatok = 11 % 3;
+		System.out.println(celoe); 
+		System.out.println(ostatok); // 3 raza vmestilos v 11 i ostalos 2 
+		
+		double f = 3.3;
+		int x = 5;
+		int a = 5;
+		System.out.println(a-- - --a + ++a + a++ + a);
+		
+		int b = 8;
+		System.out.println(++b - b++ + ++b - --b);
+	}
+}
